7b445dc5031678c007774c0fbcee7229dd038938
0xLightt <0xlightt@protonmail.com>
2025-06-23 12:34:33 +0100

    add script to fetch market cap of all OFT tokens

---
+++ scripts/marketCap/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const TOKEN_LIST = require('../../token-list.json')
+
+function getAllOFTs() {
+  return [...Object.values(TOKEN_LIST.tokens), ...Object.values(TOKEN_LIST.rootTokens)].filter((token) => token.isOFT)
+}
+
+function groupBy(array, keyFn) {
+  const map = new Map()
+
+  for (const item of array) {
+    const key = keyFn(item)
+    const keyStr = JSON.stringify(key) // Map can't use complex keys directly
+    if (!map.has(keyStr)) {
+      map.set(keyStr, [])
+    }
+    map.get(keyStr).push(item)
+  }
+
+  return Array.from(map.values())
+}
+
+function uniqueToken(token) {
+  return token.symbol
+}
+
+function getAllUniqueOFTs() {
+  return groupBy(getAllOFTs(), uniqueToken)
+}
+
+async function getOFTsPriceSource() {
+  return (await (await fetch(process.env.OFT_API)).json())[0].result.data
+}
+
+function filterOutUnusedTokens(entry, allUniqueOFTs) {
+  if (!entry.marketCap) return false
+  if (entry.project === 'Stargate' && entry.symbol !== 'STG') return false
+
+  return allUniqueOFTs.some((tokens) => {
+    return entry.symbol === tokens[0].symbol
+  })
+}
+
+async function main() {
+  const allUniqueOFTs = getAllUniqueOFTs()
+  const oftInfo = await getOFTsPriceSource()
+
+  const oftsWithPriceSource = oftInfo.filter((entry) => filterOutUnusedTokens(entry, allUniqueOFTs))
+  const marketCapSum = oftsWithPriceSource.reduce((memo, entry) => memo + entry.marketCap, 0)
+
+  console.log(`Bridgeable Market Cap from ${oftsWithPriceSource.length} tokens: ${marketCapSum}`)
+}
+
+main()
