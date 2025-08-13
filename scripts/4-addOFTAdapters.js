d8681775e8f1335bac7a6052951482327a1e1d51
0xBugsy <0xbuggy2@proton.me>
2025-08-13 22:21:48 +0100

    NIT: Remove unused

--- scripts/4-addOFTAdapters.js
+++ scripts/4-addOFTAdapters.js
@@ -1,4 +1,3 @@
-const { ethers } = require('ethers')
 const fs = require('fs')
 const { ZERO_ADDRESS } = require('maia-core-sdk')
 const { CHAIN_KEY_TO_ID, CHAIN_KEY_TO_EID, SUPPORTED_CHAINS} = require('../configs')
